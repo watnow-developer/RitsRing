655ad603f131b7853d7aa390358615e24fa22244
yu <xmas_yy@yunoMacBook-Air.local>
2019-08-18 01:07:51 +0900

    change homeVC

--- sidemenu/controller/HomeController.swift
+++ sidemenu/controller/HomeController.swift
@@ -7,12 +7,15 @@
 //
 
 
-    //メイン画面
+//メイン画面
 import UIKit
 
 class HomeController: UIViewController {
     
-
+    let image = UIImage(named:"click")!
+    var screenWidth:CGFloat = 0
+    var screenHeight:CGFloat = 0
+    let clickbutton = UIButton()
     
     // Mark - Properties
     
@@ -25,16 +28,46 @@ class HomeController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        // button
+        screenWidth = view.frame.size.width
+        screenHeight = view.frame.size.height
+        clickbutton.frame = CGRect(x:0, y:180,
+                                   width:screenWidth, height:screenWidth)
+        
+        button_init(button: clickbutton)
+        self.view.addSubview(clickbutton)
+        
+        view.backgroundColor = UIColor(red: 254/255, green: 249/255, blue: 251/255, alpha: 1)
+        
+        //画面遷移
+        clickbutton.addTarget(self, action: #selector(HomeController.click(_:)), for: .touchUpInside)
+        
+        view.addSubview(clickbutton)
+    }
+    
+    func button_init(button:UIButton){
+        clickbutton.setImage(image, for: .normal)
+        
+        
+        // Aspect Fit
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        // Horizontal 拡大
+        button.contentHorizontalAlignment = .fill
+        
+        // Vertical 拡大
+        button.contentVerticalAlignment = .fill
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         configureNavigationBar()
-
+        
     }
     
     @objc func handleMenuToggle(){
-        delegate?.handleMenuToggle(forMenuOption: nil) 
+        delegate?.handleMenuToggle(forMenuOption: nil)
+        
+        
     }
     //Mark - Handlers
     func configureNavigationBar(){
@@ -45,4 +78,11 @@ class HomeController: UIViewController {
         
         
     }
+    
+    @objc func click(_ sender: UIButton) {// selectorで呼び出す場合Swift4からは「@objc」をつける。
+        let controller1 = genderSelectController()
+        self.present(controller1, animated: true, completion: nil)
+        
+    }
+    
 }
